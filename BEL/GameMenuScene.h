7c6ebbef3895958bacb4c2555cba4dce7ed4add7
Maher Rezeq <Maher.Rezeq@Dowjones.com>
2014-03-12 20:04:12 -0400

    Added Game Menu Scene

---
+++ BEL/GameMenuScene.h
@@ -0,0 +1,13 @@
+//
+//  GameMenuScene.h
+//  BEL
+//
+//  Created by RezeqM on 3/12/14.
+//  Copyright (c) 2014 NativeNCreative. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameMenuScene : SKScene
+
+@end
---
+++ BEL/GameMenuScene.m
@@ -0,0 +1,63 @@
+//
+//  GameMenuScene.m
+//  BEL
+//
+//  Created by RezeqM on 3/12/14.
+//  Copyright (c) 2014 NativeNCreative. All rights reserved.
+//
+
+#import "GameMenuScene.h"
+
+@interface GameMenuScene()
+{
+    SKSpriteNode * _startGameButton;
+
+}
+@end
+
+@implementation GameMenuScene
+
+- (id)initWithSize:(CGSize)size
+{
+    if (self = [super initWithSize:size]) {
+        
+        [self setBackgroundColor:[SKColor colorWithRed:80.0/255.0 green:120.0/255.0 blue:255.0/255.0 alpha:1]];
+
+        // Game name label
+        SKLabelNode * label = [SKLabelNode labelNodeWithFontNamed:@"Arcade"];
+        label.text = @"Behind Enemy Lines";
+        label.fontSize = 20;
+        label.position = CGPointMake(CGRectGetMidX(self.frame),
+                                     CGRectGetMidY(self.frame)+CGRectGetMidY(self.frame)/2);
+        [self addChild:label];
+       
+        _startGameButton = [SKSpriteNode spriteNodeWithColor:[SKColor colorWithWhite:1 alpha:1] size:CGSizeMake(110, 60)];
+        [_startGameButton setColor:[SKColor colorWithRed:0.0/255.0 green:0.0/255.0 blue:0.0/255.0 alpha:1]];
+        [_startGameButton setPosition:CGPointMake(self.size.width/2, self.size.height/2)];
+        // Add button to the scene
+        [self addChild:_startGameButton];
+        
+        
+        SKLabelNode * start = [SKLabelNode labelNodeWithFontNamed:@"Arcade"];
+        start.text = @"Start";
+        start.fontSize = 30;
+        // Add label to the button
+        [_startGameButton addChild:start];
+        
+        
+    }
+    return self;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self];
+    SKNode *node = [self nodeAtPoint:location];
+    
+    if (node == _startGameButton) {
+        
+    }
+
+}
+@end
